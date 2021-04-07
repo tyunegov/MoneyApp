6973a7418c0512c52eba7a3e9d97aa9e22bbe9ac
Dmitry <dmitry_ty@list.ru>
2021-04-07 08:38:16 +0300

    feat: ApiTests fot GetAll

---
+++ MoneyAppTests/Controllers/TransactionControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Net;
+
+namespace MoneyAppAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class TransactionControllerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Arrange
+            RestClient client = new RestClient(@"https://localhost:44303/");
+            RestRequest request = new RestRequest("Transaction/All", Method.GET);
+            //Act
+            IRestResponse response = client.Execute(request);
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
