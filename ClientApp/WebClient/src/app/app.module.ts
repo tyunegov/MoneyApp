a619873264058f9bbf75de7831d75f98be505bb5
Dmitry <dmitry_ty@list.ru>
2021-04-15 19:59:29 +0300

    feat: main page

--- ClientApp/WebClient/src/app/app.module.ts
+++ ClientApp/WebClient/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { SubmenuComponent } from './submenu/submenu.component';
+import { HistoryTableComponent } from './history-table/history-table.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainContainerComponent,
+    SubmenuComponent,
+    HistoryTableComponent
   ],
   imports: [
     BrowserModule,
